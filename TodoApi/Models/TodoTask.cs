f792999017c0c195cc0af3d5cdb89e3225f218d2
marorcas <ortzmc@gmail.com>
2024-10-29 15:07:06 +1100

    Create task model

---
+++ TodoApi/Models/TodoTask.cs
@@ -0,0 +1,19 @@
+namespace TodoApi.Models {
+    public class TodoTask {
+        public long Id {
+            get; set;
+        }
+
+        public required string Name {
+            get; set;
+        }
+
+        public bool Completed {
+            get; set;
+        }
+
+        public bool Priority {
+            get; set;
+        }
+    }
+}
